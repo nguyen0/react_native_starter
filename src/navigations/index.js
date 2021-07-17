6b8de933d110206708432fe72ed7b459c48407b8
nguyen0 <nguyenkhong.hust@gmail.com>
2021-07-17 22:45:22 +0700

    react-navigation

---
+++ src/navigations/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './AuthNavigator';
+import HomeNavigator from './HomeNavigator';
+import DrawerNavigator from './DrawerNavigator';
+const AppNavContainer = () => {
+    const isLoggedIn = true;
+    return (
+        <NavigationContainer>
+            { isLoggedIn ? <DrawerNavigator/> : <AuthNavigator />}
+        </NavigationContainer>
+    )
+}
+export default AppNavContainer;
